bbd9b110e7d0691a06eab863c6c69e64da83ae9e
Jeremiah Darais <jdarais@gmail.com>
2025-12-21 13:28:03 -0800

    add schema json

--- src/datamodel.rs
+++ src/datamodel.rs
@@ -21,28 +21,55 @@ pub struct PackageInfo {
 
 pub fn schema_to_json(schema: &Schema, schema_info: &SchemaInfo) -> serde_json::Value {
     match schema {
-        Schema::Null => json!({"type": "null"}),
-        Schema::Boolean => json!({"type": "boolean"}),
-        Schema::Int => json!({"type": "int"}),
-        Schema::Long => json!({"type": "long"}),
-        Schema::Float => json!({"type": "float"}),
-        Schema::Double => json!({"type": "double"}),
-        Schema::Bytes => json!({"type": "bytes"}),
-        Schema::String => json!({"type": "string"}),
+        Schema::Null => json!({
+            "type": "null",
+            "json": schema.canonical_form(),
+        }),
+        Schema::Boolean => json!({
+            "type": "boolean",
+            "json": schema.canonical_form(),
+        }),
+        Schema::Int => json!({
+            "type": "int",
+            "json": schema.canonical_form(),
+        }),
+        Schema::Long => json!({
+            "type": "long",
+            "json": schema.canonical_form(),
+        }),
+        Schema::Float => json!({
+            "type": "float",
+            "json": schema.canonical_form(),
+        }),
+        Schema::Double => json!({
+            "type": "double",
+            "json": schema.canonical_form(),
+        }),
+        Schema::Bytes => json!({
+            "type": "bytes",
+            "json": schema.canonical_form(),
+        }),
+        Schema::String => json!({
+            "type": "string",
+            "json": schema.canonical_form(),
+        }),
         Schema::Array(array_schema) => json!({
             "type": "array",
-            "items": schema_to_json(&array_schema.items, schema_info)
+            "items": schema_to_json(&array_schema.items, schema_info),
+            "json": schema.canonical_form(),
         }),
         Schema::Map(map_schema) => json!({
             "type": "map",
-            "values": schema_to_json(&map_schema.types, schema_info)
+            "values": schema_to_json(&map_schema.types, schema_info),
+            "json": schema.canonical_form(),
         }),
         Schema::Union(union_schema) => json!({
             "type": "union",
             "variants": serde_json::Value::Array(union_schema.variants()
                 .iter()
                 .map(|s| schema_to_json(s, schema_info))
-                .collect())
+                .collect()),
+            "json": schema.canonical_form()
         }),
         Schema::Record(record_schema) => {
             if record_schema.name.fullname(None) != schema_info.full_name {
@@ -57,7 +84,7 @@ pub fn schema_to_json(schema: &Schema, schema_info: &SchemaInfo) -> serde_json::
                 for field in record_schema.fields.iter() {
                     fields.push(json!({
                         "name": field.name,
-                        "type": schema_to_json(&field.schema, schema_info)
+                        "type": schema_to_json(&field.schema, schema_info),
                     }));
                 }
     
@@ -69,7 +96,8 @@ pub fn schema_to_json(schema: &Schema, schema_info: &SchemaInfo) -> serde_json::
                     "full_name": schema_info.full_name,
                     "doc": record_schema.doc,
                     "file_path": schema_info.file_path,
-                    "fields": fields
+                    "fields": fields,
+                    "json": schema.canonical_form(),
                 })
             }
         }
@@ -79,7 +107,7 @@ pub fn schema_to_json(schema: &Schema, schema_info: &SchemaInfo) -> serde_json::
                     "type": "ref",
                     "name": enum_schema.name.name,
                     "namespace": enum_schema.name.namespace.as_ref().cloned().unwrap_or_else(String::new),
-                    "fullname": enum_schema.name.fullname(None)
+                    "fullname": enum_schema.name.fullname(None),
                 })
             } else {
                 json!({
@@ -90,7 +118,8 @@ pub fn schema_to_json(schema: &Schema, schema_info: &SchemaInfo) -> serde_json::
                     "fullname": enum_schema.name.fullname(None),
                     "doc": enum_schema.doc,
                     "symbols": enum_schema.symbols,
-                    "default": enum_schema.default
+                    "default": enum_schema.default,
+                    "json": schema.canonical_form(),
                 })
             }
         }
@@ -100,7 +129,7 @@ pub fn schema_to_json(schema: &Schema, schema_info: &SchemaInfo) -> serde_json::
                     "type": "ref",
                     "name": fixed_schema.name.name,
                     "namespace": fixed_schema.name.namespace.as_ref().cloned().unwrap_or_else(String::new),
-                    "fullname": fixed_schema.name.fullname(None)
+                    "fullname": fixed_schema.name.fullname(None),
                 })
             } else {
                 json!({
@@ -110,6 +139,7 @@ pub fn schema_to_json(schema: &Schema, schema_info: &SchemaInfo) -> serde_json::
                     "namespace": fixed_schema.name.namespace.as_ref().cloned().unwrap_or_else(String::new),
                     "fullname": fixed_schema.name.fullname(None),
                     "size": fixed_schema.size,
+                    "json": schema.canonical_form()
                 })
             }
         }
@@ -121,6 +151,7 @@ pub fn schema_to_json(schema: &Schema, schema_info: &SchemaInfo) -> serde_json::
                     obj.insert(String::from("logical_type"), serde_json::Value::String("decimal".into()));
                     obj.insert(String::from("precision"), serde_json::Value::Number(decimal_schema.precision.into()));
                     obj.insert(String::from("scale"), serde_json::Value::Number(decimal_schema.scale.into()));
+                    obj.insert(String::from("json"), serde_json::Value::String(schema.canonical_form().into()));
                     serde_json::Value::Object(obj)
                 }
                 _ => { panic!("Expected schema_to_json to return a json object"); }
@@ -128,53 +159,65 @@ pub fn schema_to_json(schema: &Schema, schema_info: &SchemaInfo) -> serde_json::
         }
         Schema::BigDecimal => json!({
             "type": "bytes",
-            "logical_type": "big-decimal"
+            "logical_type": "big-decimal",
+            "json": schema.canonical_form(),
         }),
         Schema::Uuid => json!({
             "type": "string",
             "logical_type": "uuid",
+            "json": schema.canonical_form(),
         }),
         Schema::Date => json!({
             "type": "int",
-            "logical_type": "date"
+            "logical_type": "date",
+            "json": schema.canonical_form(),
         }),
         Schema::TimeMillis => json!({
             "type": "int",
-            "logical_type": "time-millis"
+            "logical_type": "time-millis",
+            "json": schema.canonical_form(),
         }),
         Schema::TimeMicros => json!({
             "type": "long",
-            "logical_type": "time-micros"
+            "logical_type": "time-micros",
+            "json": schema.canonical_form(),
         }),
         Schema::TimestampMillis => json!({
             "type": "long",
-            "logical_type": "timestamp-millis"
+            "logical_type": "timestamp-millis",
+            "json": schema.canonical_form(),
         }),
         Schema::TimestampMicros => json!({
             "type": "long",
-            "logical_type": "timestamp-micros"
+            "logical_type": "timestamp-micros",
+            "json": schema.canonical_form(),
         }),
         Schema::TimestampNanos => json!({
             "type": "long",
-            "logical_type": "timestamp-nanos"
+            "logical_type": "timestamp-nanos",
+            "json": schema.canonical_form(),
         }),
         Schema::LocalTimestampMillis => json!({
             "type": "long",
-            "logical_type": "local-timestamp-millis"
+            "logical_type": "local-timestamp-millis",
+            "json": schema.canonical_form(),
         }),
         Schema::LocalTimestampMicros => json!({
             "type": "long",
-            "logical_type": "local-timestamp-micros"
+            "logical_type": "local-timestamp-micros",
+            "json": schema.canonical_form(),
         }),
         Schema::LocalTimestampNanos => json!({
             "type": "long",
-            "logical_type": "local-timestamp-nanos"
+            "logical_type": "local-timestamp-nanos",
+            "json": schema.canonical_form(),
         }),
         Schema::Duration => json!({
             "type": "fixed",
             "logical_type": "duration",
             "name": "DurationLogicalTYpe",
-            "size": 12
+            "size": 12,
+            "json": schema.canonical_form(),
         }),
         Schema::Ref{ name} => json!({
             "type": "ref",
