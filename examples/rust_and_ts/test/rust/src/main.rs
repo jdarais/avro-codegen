caa2199a5537c1ed13a62f994b3b5a41a5d87a17
Jeremiah Darais <jdarais@gmail.com>
2025-12-26 13:18:12 -0800

    improve rust example tests

--- examples/rust_and_ts/test/rust/src/main.rs
+++ examples/rust_and_ts/test/rust/src/main.rs
@@ -1,53 +1,57 @@
-use rust_and_ts::org::testorg::test::{RecordWithUnion, MyEnum, TestRecord};
-use rust_and_ts::_unions::Union4;
-use rust_and_ts::RecordWithRef;
 
-fn test_record_with_union() -> anyhow::Result<()> {
-    let record = RecordWithUnion {
-        some_int: 25,
-        test: Union4::Variant3(String::from("hello")),
-        enum_field: MyEnum::Yabba,
-    };
+fn main() {
 
-    // Serialize the record
-    let mut buffer: Vec<u8> = Vec::new();
-    let mut writer = RecordWithUnion::writer(&mut buffer);
-    writer.append_ser(&record)?;
-    writer.flush()?;
-    drop(writer);
-
-    // Read it back
-    let mut reader = RecordWithUnion::reader(buffer.as_slice())?;
-    let parsed_opt = RecordWithUnion::read_next(&mut reader)?;
-    let parsed = parsed_opt.unwrap();
-
-    assert_eq!(&record, &parsed);
-
-    Ok(())
 }
 
-fn test_record_with_ref() -> anyhow::Result<()> {
-    let record = RecordWithRef {
-        some_int: 45,
-        test: Some(TestRecord {
-            somefield: String::from("hi")
-        })
-    };
+mod tests {
+    use rust_and_ts::org::testorg::test::{RecordWithUnion, MyEnum, TestRecord};
+    use rust_and_ts::org::testorg::InternallyDefinedRecord;
+    use rust_and_ts::_unions::Union4;
+    use rust_and_ts::RecordWithRef;
 
-    let mut buffer: Vec<u8> = Vec::new();
-    record.write_single(&mut buffer)?;
+    #[test]
+    fn test_record_with_union() -> anyhow::Result<()> {
+        let record = RecordWithUnion {
+            some_int: 25,
+            test: Union4::Variant3(String::from("hello")),
+            enum_field: MyEnum::Yabba,
+        };
 
-    let parsed_opt = RecordWithRef::read_single(buffer.as_slice())?;
-    let parsed = parsed_opt.unwrap();
+        // Serialize the record
+        let mut buffer: Vec<u8> = Vec::new();
+        let mut writer = RecordWithUnion::writer(&mut buffer);
+        writer.append_ser(&record)?;
+        writer.flush()?;
+        drop(writer);
 
-    assert_eq!(&record, &parsed);
+        // Read it back
+        let mut reader = RecordWithUnion::reader(buffer.as_slice())?;
+        let parsed = reader.next().unwrap()?;
 
-    Ok(())
-}
+        assert_eq!(&record, &parsed);
 
-fn main() -> anyhow::Result<()> {
-    test_record_with_union()?;
-    test_record_with_ref()?;
+        Ok(())
+    }
 
-    Ok(())
+    #[test]
+    fn test_record_with_ref() -> anyhow::Result<()> {
+        let record = RecordWithRef {
+            some_int: 45,
+            test: Some(TestRecord {
+                somefield: String::from("hi")
+            })
+        };
+
+        let mut buffer: Vec<u8> = Vec::new();
+        let mut writer = RecordWithRef::writer(&mut buffer);
+        writer.append_ser(&record)?;
+        drop(writer);
+
+        let mut reader = RecordWithRef::reader(buffer.as_slice())?;
+        let parsed = reader.next().unwrap()?;
+
+        assert_eq!(&record, &parsed);
+
+        Ok(())
+    }
 }
--- src/generators/rust/generate.lua
+++ src/generators/rust/generate.lua
@@ -47,6 +47,8 @@ if params.cargo_toml then
     render("Cargo.toml.tera", "Cargo.toml")
 end
 
+render("rw.tera", "src/_rw.rs")
+
 local union_cardinalities_list = array {}
 for k, _ in pairs(union_cardinalities) do
     union_cardinalities_list:push(k)
@@ -54,6 +56,7 @@ end
 render("unions.tera", "src/_unions.rs", map { union_cardinalities = union_cardinalities_list })
 
 local lib_mod = modules:remove("")
+lib_mod.submodules["_rw"] = true
 lib_mod.submodules["_unions"] = true
 local lib_submodules = lib_mod.submodules:keys()
 table.sort(lib_submodules)
